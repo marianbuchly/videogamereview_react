4da6bda1a9f717a053c365f2bfee6cd3a1da5076
marianbuchly <marianbuchly@gmail.com>
2016-04-28 16:40:24 +0200

    jquery

--- src/Categories.js
+++ src/Categories.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import jQuery from 'jquery'
+import {Link from} 'react-router'
 
 class Categories extends React.Component {
+  contsructor(){
+    super();
+    this.state = {
+      categories: []
+    };
+  }
+
 
   componentDidMount() {
-    
+    this.getCategories();
   }
+
   getCategories(){
-    let url = "https://git.heroku.com/tranquil-dusk-52350.git"
+    let component = this;
+    let url = "https://git.heroku.com/tranquil-dusk-52350.git";
     jQuery.getJSON(url, function(data){
-      console.log(data);
+      component.setState({
+        categories: data.categories
     });
+  });
   }
 
   render() {
     return (
+      <div className="categories">
+          <h1>Categories</h1>
+          <ul>
+          {this.state.categories.map(function(category)}
+          return  (
 
-    <h1>Categories</h1>
+          <li key ={category.id}>{category.name}</li>
+          <link to ={'/categories/${category.id}'}>{category.name}</link>
+          </ul>
+        );
+      })}
 
+      </div>
 
     );
   }
